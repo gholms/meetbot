gholms/meetbot

commit 9254a4ae1e216a0fc95d07bf5405d86ee1880198

Make 'meetingname' a required argument of startmeeting.
The meetingname can be used in the filename of the meeting logs.

Change-Id: I8a72f292a6a995e517114350373507c20ca4ce7e

ircmeeting/meeting.py
@@ -314,7 +314,7 @@ class MeetingCommands(object):
         for messageline in message.split('\n'):
             self.reply(messageline)
         if line.strip():
-            self.do_meetingtopic(nick=nick, line=line, time_=time_, **kwargs)
+            self.do_meetingname(nick=nick, line=line, time_=time_, **kwargs)
     def do_endmeeting(self, nick, time_, **kwargs):
         """End the meeting."""
         if not self.isChair(nick): return

MeetBot/plugin.py
@@ -94,6 +94,10 @@ class MeetBot(callbacks.Plugin):
             if M is not None:
                 irc.error("Can't start another meeting, one is in progress.")
                 return
+            name = payload[13:].strip()
+            if not name:
+                irc.error("A meeting name is required, e.g., '#startmeeting Marketing Committee'")
+                return
             # This callback is used to send data to the channel:
             def _setTopic(x):
                 irc.sendMsg(ircmsgs.topic(channel, x))

commit f329e5f94c8aeaf0a1ce3fb1abf4c1756a346442

Fix initial meeting topic setting
Commit 9254a4ae caused meeting topics that are supplied to #startmeeting
to trigger the meetingname action in lieu of the meetingtopic action,
which means meeting topics won't actually be set unless someone
explicitly sets one later.  This commit makes it perform both actions.

ircmeeting/meeting.py
@@ -314,6 +314,7 @@ class MeetingCommands(object):
         for messageline in message.split('\n'):
             self.reply(messageline)
         if line.strip():
+            self.do_meetingtopic(nick=nick, line=line, time_=time_, **kwargs)
             self.do_meetingname(nick=nick, line=line, time_=time_, **kwargs)
     def do_endmeeting(self, nick, time_, **kwargs):
         """End the meeting."""